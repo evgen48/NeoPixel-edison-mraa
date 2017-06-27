68fda57bd83b28d3ac1be470b888b4c44050423d
Eugen Smirnov <o1o2o3o4o5@gmail.com>
2017-06-27 09:47:44 +0000

    intel copyright removed

--- examples/whitetest.js
+++ examples/whitetest.js
@@ -1,6 +1,5 @@
 /*
- * Author: Dan Yocom <dan.yocom@intel.com>
- * Copyright (c) 2014 Intel Corporation.
+ * Author: Eugene Smirnov <o1o2o3o4o5@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -25,43 +24,81 @@
 var m = require('mraa'); //require mraa
 console.log('MRAA Version: ' + m.getVersion()); //write the mraa version to the console
 
-var NPAPI = require("./build/Release/NeoPixel");
-console.log(NPAPI);
+var NPAPI = require("../src/build/Release/NeoPixel");
+//console.log(NPAPI);
 
 var channel = new NPAPI.ws2811_channel_t();
 channel.gpionum=14;
 channel.count=16;
-channel.brightness=255;
-console.log(channel);
+channel.brightness=80;
 
-var r = NPAPI.NPInit(channel);
-console.log(channel);
-console.log(channel.leds);
-console.log(channel.leds[0]);
-console.log(r);
+var gpio = NPAPI.NPInit(channel);
 
-
-var myLed = new m.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
-myLed.dir(m.DIR_OUT); //set the gpio direction to output
-var ledState = true; //Boolean to hold the state of Led
-
-var i=0;
+var i = 0;
 function periodicActivity()
 {
 
    NPAPI.NPRLedSet(channel, i, 0x000000 );
-   NPAPI.NPRender(channel, r);
+   NPAPI.NPRender(channel, gpio);
 
-   console.log("off"+i);
+//   console.log("off"+i);
    i = (i+1)%16;
 
-   NPAPI.NPRLedSet(channel, i, 0x0F0F0F );
-   NPAPI.NPRender(channel, r);
+   NPAPI.NPRLedSet(channel, i, 0xffffff );
+   NPAPI.NPRender(channel, gpio);
 
-   console.log("on"+i);
+//   console.log("on"+i);
 
-   setTimeout(periodicActivity, 300); //call the indicated function after 1 second (1000 milliseconds)
+   setTimeout(periodicActivity, 300); //call the indicated function after (100 milliseconds)
 }
 
-periodicActivity(); //call the periodicActivity function
+//periodicActivity(); ///call the periodicActivity function
+
+var myDigitalPin1 = new m.Gpio(6); //setup digital read on pin 6
+var myDigitalPin2 = new m.Gpio(7); //setup digital read on pin 6
+var myDigitalPin3 = new m.Gpio(8); //setup digital read on pin 6
+var myDigitalPin4 = new m.Gpio(9); //setup digital read on pin 6
+
+myDigitalPin1.dir(m.DIR_IN); //set the gpio direction to input
+myDigitalPin2.dir(m.DIR_IN); //set the gpio direction to input
+myDigitalPin3.dir(m.DIR_IN); //set the gpio direction to input
+myDigitalPin4.dir(m.DIR_IN); //set the gpio direction to input
+
+periodicActivityButton(); //call the periodicActivity function
+
+function periodicActivityButton() //
+{
+  var myDigitalValue1 = !myDigitalPin1.read(); //read the digital value of the pin
+  //if (myDigitalValue1)console.log('1'); //write the read value out to the console
+  var myDigitalValue2 = !myDigitalPin2.read(); //read the digital value of the pin
+  //if (myDigitalValue2)console.log('2'); //write the read value out to the console
+  var myDigitalValue3 = !myDigitalPin3.read(); //read the digital value of the pin
+ // if (myDigitalValue3)console.log('3'); //write the read value out to the console
+  var myDigitalValue4 = !myDigitalPin4.read(); //read the digital value of the pin
+  //if (myDigitalValue4)console.log('4'); //write the read value out to the console
+ 
+   for (i = 0; i < 16; i++) { 
+      NPAPI.NPRLedSet(channel, i, 0x000000 );
+   }
+
+   var bits1 = (myDigitalValue1 ? 0xFF0000 : 0);
+   var bits2 = (myDigitalValue2 ? 0xFFFF00 : 0);
+   var bits3 = (myDigitalValue3 ? 0x00FF00 : 0);
+   var bits4 = (myDigitalValue4 ? 0x0000ff : 0);
+    
+   var values = new Array(16);
+   values.fill(bits1,0,4);
+   values.fill(bits2,4,8);
+   values.fill(bits3,8,12);
+   values.fill(bits4,12,16);
+   
+   for (i = 0; i < 16; i++) { 
+      NPAPI.NPRLedSet(channel, i, values[i] );
+   }
+   
+   NPAPI.NPRender(channel, gpio);
+
+  setTimeout(periodicActivityButton,50); //call the indicated function after 1 second (1000 milliseconds)
+  
+}
 
